f2267df6584eefef1aeeaa840ffc42c50d36db30
Remi GUILLARD <remi.guillard@gmail.com>
2015-03-01 18:29:45 +0100

    Update index page to display some text

--- app/booth_app.js
+++ app/booth_app.js
@@ -1,25 +1,20 @@
-var boothApp = angular.module('boothApp', []);
+var boothApp = angular.module('TrolliBooth', []);
 
 function mainController($scope, $http) {
     $scope.formData = {};
 
-    // When getting the page, display the instagram form
-    $http.get('/')
-        .success(function(data) {
-            console.log(data);
-        })
-        .error(function(data) {
-            console.log('Error: ' + data);
-        });
+    $scope.boothname = 'TrolliBooth';
 
     // When submitting the form, connect to BoothAPI
     $scope.getFeed = function() {
         $http.post('/api/trollibooth', $scope.formData)
             .success(function(data) {
                 console.log(data);
+                $scope.feed = data;
             })
             .error(function(data) {
                 console.log('Error: ' + data);
+                $scopte.error_feed = data;
             });
     };
 
